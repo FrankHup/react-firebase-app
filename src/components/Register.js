d30653901edf757f03cf8d3b16271113269a2aa4
Francisco Javier Villilo <fvillilo@adm.unapec.edu.do>
2024-02-23 18:00:21 -0400

    Authentication-continued

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,3 +1,32 @@
-export function Register (){
-    return <div>register</div>;
+import { useState } from "react";
+
+export function Register() {
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  const handleChange = ({target:{name, value}}) => {
+    setUser({...user, [name]:value})
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="pwd"
+        placeholder="Enter your password"
+        onChange={handleChange}
+      />
+      <br />
+      <button>Register</button>
+    </form>
+  );
 }
