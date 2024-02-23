9d337a43311874579bcbe8f78e5d87214143b7ea
Francisco Javier Villilo <fvillilo@adm.unapec.edu.do>
2024-02-23 14:06:43 -0400

    configuracion-Tailwind-Add-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Routes,Route } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import "./App.css";
 
-function app(){
-  return(
-    <Routes>
-      <Route path="/" />
-    </Routes>
-  )
-} 
-    
-export default app;
+function App() {
+  return (
+    <div className="bg-slate-300 h-screen text-white flex">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
