5f1bf46442ec857a98f86ecfa50d896858c9795b
Francisco Javier Villilo <fvillilo@adm.unapec.edu.do>
2024-02-26 17:17:37 -0400

    Route-protection

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { AuthProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRouter";
 
 function App() {
   return (
     <div className="bg-slate-300 h-screen text-black flex">
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+
+          <ProtectedRoute>
+          <Home/>
+          </ProtectedRoute>
+          } />
+          
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,14 @@
 //import {useContext} from 'react';
 //import {context, useAuth} from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
 import{useAuth} from '../context/AuthContext';
 
 export function Home(){
     const {user, logout, loading}= useAuth();
-    const navigate=useNavigate();
+   
 
     const handleLogout= async()=>{
     await logout();
-    navigate('/login');
+ 
     };
 
     if (loading) return <p>Loading...</p>;
