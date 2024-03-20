1a75ca5b8247f3f0581759bc155a2daa69271ee5
Francisco Javier Villilo <fvillilo@adm.unapec.edu.do>
2024-03-20 11:06:44 -0400

    editing-registry

--- src/components/Register.js
+++ src/components/Register.js
@@ -61,7 +61,7 @@ export function Register() {
           Tipo de usuario
           </label>
             <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option disabled value={0} >Selecione una opcion</option>
+            <option value="" disabled selected hidden className="text-gray" >Selecione una opcion</option>
               <option value="Admin" placeholder="hola">Administrador</option>
               <option value="User">Usuario</option>
             </select>
